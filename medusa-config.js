e4eae2e19eee0371295f8e6020dc453764b3274c
Aleksei Stukalov <stukalov.nz@gmail.com>
2023-09-26 22:24:32 +1300

    Database Fixed

--- medusa-config.js
+++ medusa-config.js
@@ -1,37 +1,46 @@
-const dotenv = require("dotenv");
+const dotenv = require("dotenv")
 
-let ENV_FILE_NAME = "";
+let ENV_FILE_NAME = ""
 switch (process.env.NODE_ENV) {
   case "production":
-    ENV_FILE_NAME = ".env.production";
-    break;
+    ENV_FILE_NAME = ".env.production"
+    break
   case "staging":
-    ENV_FILE_NAME = ".env.staging";
-    break;
+    ENV_FILE_NAME = ".env.staging"
+    break
   case "test":
-    ENV_FILE_NAME = ".env.test";
-    break;
+    ENV_FILE_NAME = ".env.test"
+    break
   case "development":
   default:
-    ENV_FILE_NAME = ".env";
-    break;
+    ENV_FILE_NAME = ".env"
+    break
 }
 
 try {
-  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
-} catch (e) {}
+  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME })
+} catch (e) { }
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001"
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000"
 
+// Setting up the database url
+const DATABASE_USER = process.env.DATABASE_USER
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD
+const DATABASE_HOST = process.env.DATABASE_HOST
+const DATABASE_PORT = process.env.DATABASE_PORT
+const DATABASE_NAME = process.env.DATABASE_NAME
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+  `postgres://` +
+  `${DATABASE_USER}:${DATABASE_PASSWORD}` +
+  `@${DATABASE_HOST}:${DATABASE_PORT}` +
+  `/${DATABASE_NAME}`
 
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379"
 
 const plugins = [
   `medusa-fulfillment-manual`,
@@ -52,7 +61,7 @@ const plugins = [
       },
     },
   },
-];
+]
 
 const modules = {
   /*eventBus: {
@@ -67,22 +76,22 @@ const modules = {
       redisUrl: REDIS_URL
     }
   },*/
-};
+}
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
 const projectConfig = {
-  jwtSecret: process.env.JWT_SECRET,
-  cookieSecret: process.env.COOKIE_SECRET,
+  admin_cors: ADMIN_CORS,
   store_cors: STORE_CORS,
   database_url: DATABASE_URL,
-  admin_cors: ADMIN_CORS,
+  cookieSecret: process.env.COOKIE_SECRET,
+  jwtSecret: process.env.JWT_SECRET,
   // Uncomment the following lines to enable REDIS
   // redis_url: REDIS_URL
-};
+}
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
 module.exports = {
   projectConfig,
   plugins,
   modules,
-};
+}
