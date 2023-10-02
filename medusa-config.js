9df72af7c46d5e807609eab2811bb7c3609cc4f6
Aleksei Stukalov <stukalov.nz@gmail.com>
2023-10-02 19:53:27 +1300

    Admin CORS Updated

--- medusa-config.js
+++ medusa-config.js
@@ -22,8 +22,7 @@ try {
 } catch (e) {}
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+const ADMIN_CORS = process.env.ADMIN_CORS;
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
